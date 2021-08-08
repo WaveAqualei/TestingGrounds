052af9d44f9c9d119db4c8e6e41657e5b04050cc
WaveAqualei <88595598+WaveAqualei@users.noreply.github.com>
2021-08-07 22:04:01 -0500

    Update roleinfo.js

--- roleinfo.js
+++ roleinfo.js
@@ -1,12 +1,15 @@
 //colors
-var towncolor="#19FF19";
+var towncolor="#00BF00";
 var mafiacolor="red";
+var covencolor="#8000FF";
 var randcolor="#42C0FB";
 var neutcolor='lightgrey';
+var anycolor='#F5F5F5';
 var hilitecolor="orange";
 //Generic goals
 var towngoal = "Lynch every criminal and evildoer.";
 var mafiagoal = "Kill anyone that will not submit to the Mafia.";
+var covengoal = "Kill all who would oppose the Coven.";
 
 var roles=[
 			// === VANILLA ROLES ===
@@ -35,6 +38,32 @@ var roles=[
 				goal:towngoal,
 				color:towncolor
 			},
+	                {    
+				rolename:"tracker",
+				alignment:"town investigative",
+				abilities:['Track one person at night to see who they visit.'],
+				attributes:['None'],
+				goal:towngoal,
+				color:towncolor,
+			},
+	                {      
+				rolename:"spy",
+				alignment:"town investigative",
+				abilities:['You may bug a player\'s house to see what happens to them that night.'],
+				attributes:['You will know who the Mafia and Coven visit each night.'],
+				goal:towngoal,
+				color:towncolor
+			},
+	                {      
+				rolename:"psychic",
+				alignment:"town investigative",
+				abilities:['Receive a vision every night.'],
+				attributes:['On odd nights you will have a vision of three players, at least one will be Evil.',
+					'On even nights you will have a vision of two players, at least one will be Good.'
+					'All Town roles and Neutral Benign roles appear as Good, all other roles appear as Evil.'],
+				goal:towngoal,
+				color:towncolor
+			},
 	
 			// TOWN SUPPORT VANILLA
 			{      
@@ -46,7 +75,16 @@ var roles=[
 				goal:towngoal,
 				color:towncolor
 			},
-						{      
+	                {      
+				rolename:"mayor",
+				alignment:"town support",
+				abilities:['You may reveal yourself as the Mayor of the Town.'],
+				attributes:['Once you have revealed yourself as Mayor your vote counts as 3 votes.',
+					'You may not be healed once you have revealed yourself.'],
+				goal:towngoal,
+				color:towncolor
+			},
+			{      
 				rolename:"medium",
 				alignment:"town support",
 				abilities:['When dead speak to a living person at night.'],
@@ -76,28 +114,6 @@ var roles=[
 				color:towncolor
 			},
 	
-			// TOWN POWER VANILLA
-			{      
-				rolename:"mayor",
-				alignment:"town support",
-				abilities:['You may reveal yourself as the Mayor of the Town.'],
-				attributes:['Once you have revealed yourself as Mayor your vote counts as 3 votes.',
-					'You may not be healed once you have revealed yourself.'],
-				goal:towngoal,
-				color:towncolor
-			},
-			{      
-				rolename:"jailor",
-				alignment:"town killing",
-				abilities:['You may choose one person during the day to jail for the night.'],
-				attributes:['You may anonymously talk with your prisoner.',
-					'You can choose to execute your prisoner.',
-					'The jailed target can\'t perform their night ability.',
-					'If you execute a Town member, you forfeit further executions.'],
-				goal:towngoal,
-				color:towncolor
-			},
-	
 			// TOWN PROTECTIVE VANILLA
 			{      
 				rolename:"doctor",
@@ -118,9 +134,44 @@ var roles=[
 				goal:towngoal,
 				color:towncolor
 			},
+	                {      
+				rolename:"crusader",
+				alignment:"town protective",
+				abilities:['Protect one person other than yourself during the night.'],
+				attributes:['Grant your target Powerful defense.',
+					'You will know if your target is attacked.',
+					'You attack one person who visits your target on the same night.',
+					'You do not attack vampires, but you do block their attacks.],
+				goal:towngoal,
+				color:towncolor
+			},
+	                {      
+				rolename:"trapper",
+				alignment:"town protective",
+				abilities:['You may set up a Trap at another player\'s house.'],
+				attributes:['Traps take one day to build.',
+					'Traps can be torn down by selecting yourself at night.',
+					'You may only have one Trap out at a time.',
+					'Traps will trigger upon visits, but will only harm attackers.'
+					'You will know the roles of all the players that visit your trapped target.'],
+				goal:towngoal,
+				color:towncolor
+			},
+	
 	
 			// TOWN KILLING VANILLA
 			{      
+				rolename:"jailor",
+				alignment:"town killing",
+				abilities:['You may choose one person during the day to jail for the night.'],
+				attributes:['You may anonymously talk with your prisoner.',
+					'You can choose to execute your prisoner.',
+					'The jailed target can\'t perform their night ability.',
+					'If you execute a Town member, you forfeit further executions.'],
+				goal:towngoal,
+				color:towncolor
+			},
+	                {      
 				rolename:"vigilante",
 				alignment:"town killing",
 				abilities:['Choose to take justice into your own hands and shoot someone.'],
@@ -132,11 +183,11 @@ var roles=[
 			{      
 				rolename:"veteran",
 				alignment:"town killing",
-				abilities:['Decide if you gain Basic Defense.'],
-				attributes:['While on alert you can not be killed at night.',
+				abilities:['Decide if you will go on alert.'],
+				attributes:['While on alert, you gain Basic Defense.',
 					'While on alert, you will deliver a Powerful attack to anyone who visits you.',
 					'You can only go on alert 3 times.',
-					'You are immune to role blocks.'],
+					'You cannot be roleblocked.'],
 				goal:towngoal,
 				color:towncolor
 			},
@@ -145,12 +196,10 @@ var roles=[
 			{      
 				rolename:"godfather",
 				alignment:"mafia killing",
-				abilities:['Kill someone each night.'],
-				attributes:['You can\'t be killed at night.',
-					'You can choose to command other member to kill for you.',
+				abilities:['You may choose to attack a player each night.'],
+				attributes:['If there is a Mafioso they will attack the target instead of you.',
 					'You will appear to be innocent to the Sheriff.',
-					'You can talk with the other Mafia at night.',
-					'You will receive silent messages which your fellow Mafiosi aren\'t given'],
+					'You can talk with the other Mafia at night.'],
 				goal:mafiagoal,
 				color:mafiacolor
 			},
@@ -163,7 +212,6 @@ var roles=[
 					'You can talk with the other Mafia at night.'],
 				goal:mafiagoal,
 				color:mafiacolor,
-				custom:true
 			},
 	                {      
 				rolename:"ambusher",
@@ -256,6 +304,70 @@ var roles=[
 				color:mafiacolor
 			},
 	
+	                // COVEN VANILLA
+	                {      
+				rolename:"coven leader",
+				alignment:"coven evil",
+				abilities:['You may choose to Control someone each night.'],
+				attributes:['Your victim will know they are being controlled.',
+					'With the Necronomicon, your victim is dealt a Basic attack and you gain Basic defense.',
+					'You will know the role of the player you control.'],
+				goal:covengoal,
+				color:covencolor
+			},
+	                {      
+				rolename:"hex master",
+				alignment:"coven evil",
+				abilities:['You may choose to Hex a player each night.'],
+				attributes:['Players are not notified upon being hexed.',
+					'Hexed targets will have their investigative results changed.',
+					'When all living non-Coven players are hexed, all hexed players will be dealt an Unstoppable attack.',
+					'With the Necronomicon, you gain Astral and Basic attacks.'],
+				goal:covengoal,
+				color:covencolor
+			},
+	                {      
+				rolename:"medusa",
+				alignment:"coven evil",
+				abilities:['You may choose to Stone Gaze all visitors at night.'],
+				attributes:['You may choose to stone gaze thrice.',
+					'Your victims\'s last wills and roles will not be revealed.',
+					'With the Necronomicon, you may visit players and turn them to stone.'],
+				goal:covengoal,
+				color:covencolor
+			},
+	                {      
+				rolename:"necromancer",
+				alignment:"coven evil",
+				abilities:['You may reanimate a dead player and use their ability on a player.'],
+				attributes:['Create zombies from dead players who use their abilities on your second target.',
+					'Each zombie can be used once before it rots.',
+					'With the Necronomicon, select yourself to summon a ghoul to Basic attack your target.'],
+				goal:covengoal,
+				color:covencolor
+			},
+	                {      
+				rolename:"poisoner",
+				alignment:"coven evil",
+				abilities:['You may choose to poison a player each night.'],
+				attributes:['Your poisons take one day to take effect.',
+					'Poison can be removed by Heals.',
+					'With the Necronomicon, your poison can no longer be Healed.'],
+				goal:covengoal,
+				color:covencolor
+			},
+	                {      
+				rolename:"potion master",
+				alignment:"coven evil",
+				abilities:['You may choose to use a potion on a player each night.'],
+				attributes:['You may choose to use a Heal, reveal, or attack potion on a player.',
+					'Each potion has a three day cooldown.',
+					'With the Necronomicon, your potions no longer have a cooldown.'],
+				goal:covengoal,
+				color:covencolor
+			},
+	
+	
 			// NEUTRAL BENIGN VANILLA
 			{      
 				rolename:"survivor",
@@ -263,7 +375,7 @@ var roles=[
 				abilities:['Put on a bulletproof vest at night, granting you Basic Defense.'],
 				attributes:['You can only use the bulletproof vest 4 times.'],
 				goal:"Live to the end of the game.",
-				color:"#DDDD00"
+				color:"#C8C800"
 			},
 			{      
 				rolename:"amnesiac",
@@ -271,7 +383,17 @@ var roles=[
 				abilities:['Remember who you were by selecting a graveyard role.'],
 				attributes:['When you choose a role it will be revealed to all the players in the game.'],
 				goal:"Remember who you were and complete that roles objectives.",
-				color:"cyan"
+				color:"#44A6C6"
+			},
+	                {      
+				rolename:"guardian angel",
+				alignment:"neutral benign",
+				abilities:['Keep your target alive.'],
+				attributes:['Your target can be any player except an Executioner, Jester, or another Guardian Angel.',
+					'If your target is killed you will become a Survivor without any bulletproof vests.',
+					'Twice a game you may Heal and Purge your target. This may be done from the grave. Watching over a player ignores Jail.'],
+				goal:"Keep your target alive until the end of the game.",
+				color:"#FFFFFF"
 			},
 	
 			// NEUTRAL EVIL VANILLA
@@ -281,16 +403,16 @@ var roles=[
 				abilities:['Trick the Town into voting against you.'],
 				attributes:['If you are lynched you may kill one of your guilty or abstaining voters the following night.'],
 				goal:"Get yourself lynched by any means necessary.",
-				color:"pink"
+				color:"#F49FD0"
 			},
 			{      
 				rolename:"executioner",
 				alignment:"neutral evil",
 				abilities:['Trick the Town into lynching your target.'],
-				attributes:['Your target is <u>NAMEOFTARGET</u>.',
+				attributes:['Your target can be any Townmember except a Jailor or Mayor.',
 					'If your target is killed at night you will become a jester.'],
 				goal:"Get your target lynched at any cost.",
-				color:"grey"
+				color:"#ACACAC"
 			},
 			{      
 				rolename:"witch",
@@ -312,31 +434,72 @@ var roles=[
 					'Role blockers that target you will have their last will covered in blood making it unreadable.',
 					'You can choose to be cautious and not kill role blockers.'],
 				goal:"Kill everyone who would oppose you.",
-				color:"blue"
+				color:"#000080"
 			},
 			{      
 				rolename:"arsonist",
 				alignment:"neutral killing",
 				abilities:['Douse someone in gasoline or ignite all doused targets.'],
-				attributes:['You will douse your roleblocker.',
-					'Death from fire can\'t be prevented by healing or night immunities.',
-					'A doused target will be framed as Arsonist until they die.',
-					'Select yourself to ignite doused people.',
-					'You can not be killed at night.'],
+				attributes:['Select yourself to ignite doused people dealing an Unstoppable attack.',
+					'You will douse anybody that visits you.',
+					'If you take no action, you will attempt to clean gasoline off yourself.',
+					'Doused targets will have their investigative results changed. Players will not know they were doused.'],
 				goal:"Live to see everyone else burn.",
-				color:"orange"
+				color:"#EE7600"
 			},
 			{      
 				rolename:"werewolf",
 				alignment:"neutral killing",
 				abilities:['Transform into a Werewolf during the full moon.'],
-				attributes:['You can not be killed at night.',
-					'As a Werewolf you will attack your victim and anyone that visits them.',
-					'Your attack goes through night immunity.'],
+				attributes:['You will Rampage at a player\'s house when you attack.',
+					'If you do not select a target you will stay home and Rampage at your home.'],
 				goal:"Kill everyone who would oppose you.",
-				color:"brown"
+				color:"#361E01"
 			},
-			
+	                {      
+				rolename:"juggernaut",
+				alignment:"neutral killing",
+				abilities:['You may choose to attack a player on Full-Moon nights.'],
+				attributes:['With each kill your powers grow.',
+					'On your first kill, you may attack every night.',
+					'On your second kill, you Rampage when you attack.',
+					'On your third kill, you ignore all effects that would protect a player.'],
+				goal:"Kill everyone who would oppose you.",
+				color:"#631A35"
+			},
+	
+	                // NEUTRAL CHAOS VANILLA
+			{      
+				rolename:"pirate",
+				alignment:"neutral chaos",
+				abilities:['Choose a player to plunder each night.'],
+				attributes:['When you plunder a player, you will duel the player for their valuables.',
+					'If the player defends against your attack, you get no loot.'],
+				goal:"Successfully plunder two players.",
+				color:"#DBAD59"
+			},
+	                {      
+				rolename:"plaguebearer",
+				alignment:"neutral chaos",
+				abilities:['You may choose to infect a player with the Plague each night.'],
+				attributes:['Infected players spread the Plague on visiting or being visited.',
+					'Infection cannot be protected against or removed.',
+					'Players will not know they have been infected.',
+					'When all living players are infected, you will become Pestilence.'],
+				goal:"Infect all living players and become Pestilence.",
+				color:"#CFFF63"
+			},
+	                {      
+				rolename:"pestilence",
+				alignment:"neutral chaos",
+				abilities:['You may choose to Rampage at a player\'s house eacn night.'],
+				attributes:['You will attack anyone that visits you or your target.',
+					'You cannot be roleblocked or controlled.',
+					'If you are jailed you will attack the Jailor.',
+					'You cannot be killed at night.'],
+				goal:"Kill all who would oppose you.",
+				color:"#010302"
+			},
 
 			// === CUSTOM ROLES ====
 			// TOWN INVESTIGATIVE CUSTOM
@@ -357,15 +520,6 @@ var roles=[
 				alignment:"town investigative",
 				abilities:['Interview 2 people each night and discover who is the most trustworthy.'],
 				attributes:['You cannot interview a revealed Mayor.'],
-				goal:towngoal,
-				color:towncolor,
-				custom:true
-			},
-			{    
-				rolename:"tracker",
-				alignment:"town investigative",
-				abilities:['Follow one person to see who they visit.'],
-				attributes:['None'],
 				goal:towngoal,
 				color:towncolor,
 				custom:true
@@ -406,19 +560,7 @@ var roles=[
 				color:towncolor,
 				custom:true
 			},
-			// TOWN POWER CUSTOM:
-			{      
-				rolename:"psychic",
-				alignment:"town power",
-				abilities:['Form a mental link between two players every day.'],
-				attributes:['They will be able to speak with each other that night.',
-					'You get one self link.',
-					'Roles with a night chat can hear and speak to both the person that they are linked to and whoever they speak to at night.'],
-				goal:towngoal,
-				color:towncolor,
-				custom:true
-			},
-	
+			
 			// TOWN PROTECTIVE CUSTOM:
 			{    
 				rolename:"ghost",
@@ -441,7 +583,7 @@ var roles=[
 				alignment:"Town Killing",
 				abilities:['Cast your line into someone’s house each night.'],
 				attributes:['If someone visits your target, they will be “hooked”.',
-					'You will know you hooked someone, and they will know they were hooked. ',
+					'You will know you hooked someone, and they will know they were hooked.',
 					'You may decide to ‘release’, or ‘kill’ your hooked player the night following a successful hook.'
 				],
 				goal:towngoal,
@@ -469,20 +611,6 @@ var roles=[
 					'Targets are told who the nightmare is about.',
 					'Your target will be unable to visit the person until you give them a different nightmare.',								
 					'All nightmares end if the nightmarer dies or is promoted.'
-				],
-				goal:mafiagoal,
-				color:mafiacolor,
-				custom:true
-			},
-			{      
-				rolename:"drug dealer",
-				alignment:"mafia deception",
-				abilities:['Choose to drug someone at night.'],
-				attributes:['Drugged targets will get a fake notification of your choice.',
-					'You can choose between dousing, roleblocking, transporting, healing, guarding and witching.',
-					'Alternatively, you can stop them from getting notifications for that night.',
-					'You cannot use the same drug in a row.'
-				//'If there are no kill capable Mafia roles left you will become a Mafioso.'
 				],
 				goal:mafiagoal,
 				color:mafiacolor,
@@ -661,23 +789,24 @@ var roles=[
 				color:towncolor,
 				custom:true
 			},
-			{
-				rolename:"hunter",
-				alignment:"town casual",
-				abilities:['Your only ability is your vote.'],
-				attributes:['Upon your lynch, you will be able to kill any player in the game.',
-					    'If another Hunter is lynched first, you will miss the hunt, put away your rifle, and become a Citizen.'],
-				goal:towngoal,
-				color:towncolor,
+	                {
+				rolename:"cleaned",
+				alignment:"random dead",
+				abilities:['None.'],
+				attributes:['You were cleaned by a Janitor.',
+					'Your Role and Last Will could not be determined.'],
+				goal:"Complete your original goal while dead.",
+				color:anycolor,
 				custom:true
 			},
-			{
-				rolename:"goon",
-				alignment:"mafia casual",
-				abilities:['Your only ability is your vote.'],
-				attributes:['You can talk with the Mafia at night.', 'Cannot receive the Caporegime modifier.'],
-				goal:mafiagoal,
-				color:mafiacolor,
+	                {
+				rolename:"stoned",
+				alignment:"random dead",
+				abilities:['None.'],
+				attributes:['You were stoned by Medusa.',
+					'Your Role and Last Will could not be determined.'],
+				goal:"Complete your original goal while dead.",
+				color:anycolor,
 				custom:true
 			},
 			{
@@ -686,7 +815,7 @@ var roles=[
 				abilities:['Code and Host the TG.'],
 				attributes:['You are the fastest ever to live!'],
 				goal:"Make TG a happy place.",
-				color:"#0000FF",
+				color:"#000FFF",
 				custom:true
 			},
 			{
@@ -718,7 +847,7 @@ var roles=[
 			},
 	
 ];
-var unique = ["jailor", "mayor", "retributionist", "veteran", "godfather", "mafioso", "werewolf", "ghost", "warlock", "rain dancer", "musician", "psychic"];
+var unique = ["jailor", "mayor", "retributionist", "veteran", "godfather", "mafioso", "ambusher", "juggernaut", "werewolf", "coven leader", "medusa", "potion master", "hex master", "necromancer", "poisoner", "pirate", "plaguebearer", "pestilence", "ghost", "warlock", "rain dancer", "musician"];
  
 function getAttributes(num)
 {
