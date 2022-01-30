WaveAqualei/TestingGrounds

commit cbc79b1a39649f02d255e29570a9a1d2d4889cea

Move spectators to the end of the list, and style them to look different from living players

socketstuff.js
@@ -675,13 +675,36 @@ socket.on(Type.ROOMLIST,function(list)
 			{
 				$('#userlist').append(`<li><div class="info" id="p-${list[i].name}"><span class="num">${num}</span>${name}</div></li>`);
 			}
+			if(list[i].spectate)
+			{
+				$($('#userlist').children()[i]).addClass('spectator');
+			}
 
 			users.push(list[i].name);
 		}
 	}
-	if (mod)
+	else
 	{
-		$('.name').addClass('shorten');
+		var user_names = {};
+		$('#userlist').children().each((i,el)=>user_names[users[i]] = el);
+		users = [];
+		$('#userlist').empty();
+		for(i in list)
+		{
+			if(user_names[list[i].name])
+			{
+				$('#userlist').append(user_names[list[i].name]);
+				users.push(list[i].name);
+			}
+			else
+			{
+				listeners[Type.JOIN](list[i].name);
+			}
+			if(list[i].spectate)
+			{
+				$($('#userlist').children()[i]).addClass('spectator');
+			}
+		}
 	}
 });
 socket.on(Type.TOGGLELIVING,function(p)
@@ -1126,11 +1149,11 @@ socket.on(Type.SETDEV,function(name)
 });
 socket.on(Type.SETSPEC, function (name) {
 	var index = users.indexOf(name);
-	$($('.name')[index]).addClass('spec');
+	$($('#userlist').children()[index]).addClass('spectator');
 });
 socket.on(Type.REMSPEC, function (name) {
 	var index = users.indexOf(name);
-	$($('.name')[index]).removeClass('spec');
+	$($('#userlist').children()[index]).removeClass('spectator');
 });
 socket.on(Type.ROLECARD,function(card)
 {

playstyle.css
@@ -510,11 +510,6 @@ header ul li span
 	font-weight:bold;
 	color:#00a6a6;
 }
-.spec
-{
-	font-weight:bold;
-	color:#B5A642;
-}
 #inputarea
 {
 	clear:both;
@@ -935,6 +930,16 @@ header ul li span
 {
 	width:100%;
 	height:50%;
+}
+.spectator
+{
+	background:#cccccc;
+	opacity: 0.5;
+}
+.spectator .name
+{
+	font-weight:bold;
+	color:#B5A642;
 }
 .blocker
 {

server.js
@@ -650,6 +650,7 @@ io.on('connection', function (socket) {
 			for (i in playernums) {
 				var p = {};
 				p.name = players[playernums[i]].name;
+				p.spectate = players[playernums[i]].spectate;
 				if (!players[playernums[i]].alive) {
 					p.role = players[playernums[i]].role;
 					p.rolecolor = roles.getRoleData(players[playernums[i]].role).color;
@@ -682,6 +683,7 @@ io.on('connection', function (socket) {
 				for (i in playernums) {
 					var p = {};
 					p.name = players[playernums[i]].name;
+					p.spectate = players[playernums[i]].spectate;
 					if (!players[playernums[i]].alive) {
 						p.role = players[playernums[i]].role;
 						p.rolecolor = roles.getRoleData(players[playernums[i]].role).color;
@@ -1309,6 +1311,26 @@ function setPhase(p) {
 				players[i].seancing = undefined;
 			}
 		}
+	} else if(phase == Phase.PREGAME && p != Phase.PREGAME) {
+		//Game start!
+		//Send all spectators to the end of the list
+		var notspec = playernums.filter(i=>!players[i].spectate);
+		var spec = playernums.filter(i=>players[i].spectate);
+		playernums = notspec.concat(spec);
+
+		//Resend the list.
+		var namelist = playernums.map(function(id) {
+			var p = {};
+			p.name = players[id].name;
+			p.spectate = players[id].spectate;
+			if (!players[id].alive) {
+				p.role = players[id].role;
+				p.rolecolor = roles.getRoleData(players[id].role).color;
+				p.haswill = !!players[id].publicwill;
+			}
+			return p;
+		});
+		io.emit(Type.ROOMLIST, namelist);
 	}
 	phase = p;
 	timer.setPhase(p);

commit 5b07be8384b49b19adac4ba86f5820d1ebeef3b1

Fixed spectator movement scrambling player numbers for mod
Bug introduced in revision cbc79b1a39649f02d255e29570a9a1d2d4889cea

socketstuff.js
@@ -590,6 +590,7 @@ addSocketListener(Type.ROOMLIST,function(list)
 		{
 			if(user_names[list[i].name])
 			{
+				$(user_names[list[i].name]).find('.num').text(+i || 'MOD');
 				$('#userlist').append(user_names[list[i].name]);
 				delete user_names[list[i].name];
 				users.push(list[i].name);