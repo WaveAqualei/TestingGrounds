72a8e4c346b90c1402fe02c7c89faf45645527cb
Wave <88595598+WaveAqualei@users.noreply.github.com>
2022-02-22 00:36:10 -0600

    Update common.js

--- common.js
+++ common.js
@@ -369,6 +369,9 @@ addMessageHandler(Type.PRENOT,function(notification)
 	  case 'GUARDIAN_ANGEL':
 		return processMessage({msg: "The Guardian Angel was watching over you!", styling: 'reviving'}, 'prenot');
 		break;
+	  case 'CARNATION':
+		return processMessage({msg: "A Carnation has purified you!", styling: 'reviving'}, 'prenot');
+		break;
 	  case 'TRANSPORT':
 		return processMessage({msg: "You were transported to another location.", styling: 'dying'}, 'prenot');
 		break;
