WaveAqualei/TestingGrounds

commit 35e0da128610704e798452335205fbe34cdae86d

Hide the voting and judgment interfaces for dead players and spectators, and remove the button to vote up yourself

socketstuff.js
@@ -788,12 +788,12 @@ else
 			addPauseButton(phase);
 		}
 	}
-	if (phase == 4 && !mod) //Voting
+	if (phase == 4 && !mod && !$(`#p-${player_name}`).closest('li').is('.deadplayer, .spectator')) //Voting
 	{
 		//Add the voting interface
 		for (i = 1; i < users.length; i++)
 		{
-			if (!$($('#userlist li')[i]).hasClass('deadplayer') && !$($('#userlist li')[i]).find('.name.spec').length && !$($('#userlist li')[i]).find('.angel').length)
+			if (!$($('#userlist li')[i]).is('.deadplayer, .spectator') && !$($('#userlist li')[i]).find('.angel').length && users[i] !== player_name)
 			{
 				var li = $('#userlist').children()[i];
 				var button = $('<div class="votebutton">Vote</div>');
@@ -811,7 +811,7 @@ else
 			}
 		}
 	}
-	if (phase == 6 && !mod) //Verdicts, guilty/inno/abstain
+	if (phase == 6 && !mod && !$(`#p-${player_name}`).closest('li').is('.deadplayer, .spectator')) //Verdicts, guilty/inno/abstain
 	{
 		//Add verdict interface
 		var verdict = $('<div class="verdictinterface"></div>');

commit 91d43581c0cb86dfa84b46664369d27eb474b49f

Restored the ability to see the vote count on yourself
Bug introduced in revision 35e0da128610704e798452335205fbe34cdae86d

Also restored the vote count on angel-protected players (though this will always be zero).

playstyle.css
@@ -607,6 +607,7 @@ header ul li span
 	float: right;
 	height: 100%;
 	width: 100px;
+	text-align: right;
 }
 .nightinterface
 {
@@ -618,7 +619,7 @@ header ul li span
 {
 	width:70%;
 	height:110%;
-	float:left;
+	display:inline-block;
 	background: linear-gradient(teal, mediumaquamarine);
 	border-radius:8px;
 	font-family: 'Slabo 27px', serif;
@@ -673,7 +674,7 @@ header ul li span
 	border:1px solid black;
 	margin:-1px;
 	border-radius:8px;
-	float:left;
+	display:inline-block;
 	text-align:center;
 	background:tan;
 }

socketstuff.js
@@ -793,19 +793,21 @@ else
 		//Add the voting interface
 		for (i = 1; i < users.length; i++)
 		{
-			if (!$($('#userlist li')[i]).is('.deadplayer, .spectator') && !$($('#userlist li')[i]).find('.angel').length && users[i] !== player_name)
+			if (!$($('#userlist li')[i]).is('.deadplayer, .spectator'))
 			{
 				var li = $('#userlist').children()[i];
-				var button = $('<div class="votebutton">Vote</div>');
-				button.click(function()
-				{
-					var index = $('#userlist li').index(this.parentNode.parentNode.parentNode);
-					var name = users[index];
-					socket.sendMessage(Type.VOTE,name);
-				});
-				var count = $('<div class="votecount">0</div>');
 				var votinginterface = $('<div class="votinginterface"></div>');
-				votinginterface.append(button);
+				if(users[i] !== player_name && !$($('#userlist li')[i]).find('.angel').length) {
+					var button = $('<div class="votebutton">Vote</div>');
+					button.click(function()
+					{
+						var index = $('#userlist li').index(this.parentNode.parentNode.parentNode);
+						var name = users[index];
+						socket.sendMessage(Type.VOTE,name);
+					});
+					votinginterface.append(button);
+				}
+				var count = $('<div class="votecount">0</div>');
 				votinginterface.append(count);
 				$($(li).children()[0]).append(votinginterface);
 			}