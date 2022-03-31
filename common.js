WaveAqualei/TestingGrounds

commit 605f2a4d730c8ad2c94381b306a3bd02c2f9d8f0

Put a stop to roleless players being referred to in the plural in targeting messages

common.js
@@ -186,6 +186,10 @@ function processMessage(msg, type)
 				{
 					msg.role = '('+msg.role+') is';
 				}
+				else if(msg.role === '')
+				{
+					msg.role = ' is';
+				}
 				else
 				{
 					msg.role = ' are';

commit 2164b9e35e805fa92ebfbb760a4ecbb5cb774e8d

Fixed "You" being referred to in the singular in targeting messages
Bug introduced in revision 605f2a4d730c8ad2c94381b306a3bd02c2f9d8f0

common.js
@@ -480,7 +480,14 @@ addMessageHandler(Type.PRENOT,function(notification)
 });
 addMessageHandler(Type.TARGET,function(name,role,target)
 {
-	return processMessage({name:name,role:sanitize(role),target:target},'target');
+	if(typeof role === 'string') {
+		role = sanitize(role);
+	}
+	return processMessage({
+		name,
+		role,
+		target,
+	},'target');
 });
 addMessageHandler(Type.MAYOR, function(name) {
 	return processMessage({msg: '🎩' +name+' has revealed themselves as the Mayor!', styling: 'mayor_reveal'}, "highlight");